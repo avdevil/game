6e0c45ba749bff38981558795c7cdb186a3081cf
avdevil <60883474+avdevil@users.noreply.github.com>
2020-02-26 21:01:12 +0530

    Update Banner.py

--- Banner.py
+++ Banner.py
@@ -1,8 +1,8 @@
 import banner
 
- print(r"      ,
+ print(r"      ,.                      ")
  print(r"      |'.             , ...  ")
- print(r"      |  '-._        / ).    ")
+ print(r"      |  '-._        / ).    
  print(r"     .'  .._  ',     /_'-,.    ")
  print(r"     '   /  _'.'_\   /._)').    ")
  print(r"         :   /  '_' '_'  /  _.'")
