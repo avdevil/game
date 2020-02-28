725e9c710c43f64592d82eee47fd885f7c3a02ea
avdevil <60883474+avdevil@users.noreply.github.com>
2020-02-28 21:38:26 +0530

    Update Banner.py

--- Banner.py
+++ Banner.py
@@ -1,5 +1,10 @@
-import requests
-import banner
+import platform
+import os
+import time
+import random
+import urllib.request
+
+def banner():
 
  print(r"************************************** ")
  print(r"*******#### coded by avhacker #******* ")
