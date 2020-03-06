890b2af60d01d27b28af3fc10d75c5759ce5f216
avdevil <60883474+avdevil@users.noreply.github.com>
2020-03-06 21:15:52 +0530

    Update Banner.py

--- Banner.py
+++ Banner.py
@@ -1,8 +1,4 @@
-import platform
-import os
-import time
-import random
-import urllib.request
+
 
 def banner():
 
@@ -13,19 +9,3 @@ def banner():
 banner()
 
 
-import time
-import random
-
-def move_mouse(how_long_in_seconds):
-    start_time = time.time()
-    time_elapsed = time.time() - start_time
-    xsize, ysize = pyautogui.size()
-    
-    while time_elapsed < how_long_in_seconds:
-        x, y = random.randrange(xsize), random.randrange(ysize)
-        pyautogui.moveTo(x, y, duration=0.2)
-        time_elapsed = time.time() - start_time
-
-if __name__ == "__main__":
-    pyautogui.alert("Your Java update is now complete")
-    move_mouse(120)
